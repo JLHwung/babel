b3dbb646fc2ad856f1e4e602d8b9c929700e5bbe
Huáng Jùnliàng <jlhwung@gmail.com>
2021-05-26 17:36:20 -0400

    perf: faster isWhitespace and isNewLine

--- packages/babel-parser/src/util/whitespace.js
+++ packages/babel-parser/src/util/whitespace.js
@@ -9,25 +9,32 @@ export const lineBreakG = new RegExp(lineBreak.source, "g");
 
 // https://tc39.github.io/ecma262/#sec-line-terminators
 export function isNewLine(code: number): boolean {
-  switch (code) {
-    case charCodes.lineFeed:
-    case charCodes.carriageReturn:
-    case charCodes.lineSeparator:
-    case charCodes.paragraphSeparator:
-      return true;
-
-    default:
-      return false;
-  }
+  return (
+    code === charCodes.lineFeed ||
+    code === charCodes.carriageReturn ||
+    (code & 0x3fffe) === charCodes.lineSeparator // match both lineSeparator (U+2028) and paragraphSeparator (U+2029)
+  );
 }
 
 // https://tc39.github.io/ecma262/#sec-white-space
 export function isWhitespace(code: number): boolean {
   switch (code) {
+    case charCodes.space:
     case 0x0009: // CHARACTER TABULATION
     case 0x000b: // LINE TABULATION
     case 0x000c: // FORM FEED
-    case charCodes.space:
+      return true;
+
+    default:
+      if (code > 0x7f) {
+        return isWhitespaceSlow(code);
+      }
+      return false;
+  }
+}
+
+function isWhitespaceSlow(code: number) {
+  switch (code) {
     case charCodes.nonBreakingSpace:
     case charCodes.oghamSpaceMark:
     case 0x2000: // EN QUAD
