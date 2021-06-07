760506c12ef41fc2f24f1e1aa78da59a7f306a51
J. S. Choi <jschoi@jschoi.org>
2021-06-07 17:33:19 -0400

    parser: Disable Hack pipes + v8intrinsic/placeholders We may reenable combining these again if TC39 ends up deciding to use % as a topic token. See https://github.com/babel/babel/pull/13416#issuecomment-856104893.

--- packages/babel-parser/src/plugins/v8intrinsic.js
+++ packages/babel-parser/src/plugins/v8intrinsic.js
@@ -6,7 +6,7 @@ export default (superClass: Class<Parser>): Class<Parser> =>
   class extends superClass {
     parseV8Intrinsic(): N.Expression {
       if (this.match(tt.modulo)) {
-        const start = this.state.start;
+        const v8IntrinsicStart = this.state.start;
         // let the `loc` of Identifier starts from `%`
         const node = this.startNode();
         this.eat(tt.modulo);
@@ -18,32 +18,7 @@ export default (superClass: Class<Parser>): Class<Parser> =>
             return identifier;
           }
         }
-
-        // In this case, the `%` currently being parsed is not followed by an identifier,
-        // so it therefore cannot be a V8 intrinsic.
-        // If the `pipelineOperator` plugin is active,
-        // then the `%` currently being parsed may instead be a topic reference.
-        const pipeProposal = this.getPluginOption(
-          "pipelineOperator",
-          "proposal",
-        );
-        if (pipeProposal) {
-          // A pipe-operator proposal is active,
-          // although its configuration might not match `%` as a topic token.
-          // If the pipe-operator plugin’s configuration matches the current token’s type,
-          // then this will return `node`, will have been finished as a topic reference.
-          // Otherwise, this will throw a `PipeTopicUnconfiguredToken` error.
-          return this.finishTopicReference(
-            node,
-            start,
-            pipeProposal,
-            tt.modulo,
-          );
-        } else {
-          // A pipe-operator proposal is not active.
-          // Throw a fatal syntax error.
-          this.unexpected(start);
-        }
+        this.unexpected(v8IntrinsicStart);
       }
     }
 
