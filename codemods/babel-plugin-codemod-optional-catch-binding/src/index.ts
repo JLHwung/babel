9553c145554a84950af81eccfec8acee014591a3
Huáng Jùnliàng <jlhwung@gmail.com>
2022-08-24 13:12:14 -0400

    codemods

--- codemods/babel-plugin-codemod-optional-catch-binding/src/index.ts
+++ codemods/babel-plugin-codemod-optional-catch-binding/src/index.ts
@@ -1,5 +1,7 @@
 import syntaxOptionalCatchBinding from "@babel/plugin-syntax-optional-catch-binding";
 import type { PluginAPI, PluginObject } from "@babel/core";
+import type { NodePath } from "@babel/traverse";
+import type * as t from "@babel/types";
 
 export default function ({ types: t }: PluginAPI): PluginObject {
   return {
@@ -10,12 +12,12 @@ export default function ({ types: t }: PluginAPI): PluginObject {
         if (path.node.param === null || !t.isIdentifier(path.node.param)) {
           return;
         }
-        const binding = path.scope.getOwnBinding(path.node.param.name);
+        const binding = path.scope.getOwnBinding(path.node.param.name)!;
         if (binding.constantViolations.length > 0) {
           return;
         }
         if (!binding.referenced) {
-          const paramPath = path.get("param");
+          const paramPath = path.get("param") as NodePath<t.Identifier>;
           paramPath.remove();
         }
       },
